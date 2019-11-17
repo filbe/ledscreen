dec2306ab2358c404622df39d62667d62259b857
V-P Lahti <vilp3@hotmail.com>
2019-11-17 18:35:27 +0200

    rpi first test

--- drivers/rpi.c
+++ drivers/rpi.c
@@ -1,11 +1,55 @@
+#ifndef LCD_RPI_DRIVER_C
+#define LCD_RPI_DRIVER_C
+
 #ifdef RPI
 
 #include <time.h>
 #include <unistd.h>
 #include "rpi.h"
 
+void __LCD_DRIVER_INIT() {
+	wiringPiSetup();
+	pinMode(0, OUTPUT);
+}
+void __LCD_DRIVER_WRITE_ZERO_SIG_UP() {
+	digitalWrite(0, 1);
+}
+
+void __LCD_DRIVER_WRITE_ZERO_SIG_UP_AFTER() {
+	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
+}
+
+void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN() {
+	digitalWrite(0, 0);
+}
+
+void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN_AFTER() {
+	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
+}
+
+void __LCD_DRIVER_WRITE_ONE_SIG_UP() {
+	digitalWrite(0, 1);
+}
+
+void __LCD_DRIVER_WRITE_ONE_SIG_UP_AFTER() {
+	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
+}
+
+void __LCD_DRIVER_WRITE_ONE_SIG_DOWN() {
+	digitalWrite(0, 0);
+}
+
+void __LCD_DRIVER_WRITE_ONE_SIG_DOWN_AFTER() {
+	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
+}
+
+void __LCD_DRIVER_WRITE_RESET() {
+	digitalWrite(0, 0);
+}
+
 void __LCD_DRIVER_WRITE_RESET_AFTER() {
 	usleep(51);
 }
 
 #endif
+#endif /*LCD_RPI_DRIVER_C */
--- drivers/rpi.h
+++ drivers/rpi.h
@@ -5,45 +5,16 @@
 #include <wiringPi.h>
 
 
-void __LCD_DRIVER_INIT() {
-	wiringPiSetup();
-	pinMode(0, OUTPUT);
-}
-void __LCD_DRIVER_WRITE_ZERO_SIG_UP() {
-	digitalWrite(0, 1);
-}
-
-void __LCD_DRIVER_WRITE_ZERO_SIG_UP_AFTER() {
-	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
-}
-
-void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN() {
-	digitalWrite(0, 0);
-}
-
-void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN_AFTER() {
-	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
-}
-
-void __LCD_DRIVER_WRITE_ONE_SIG_UP() {
-	digitalWrite(0, 1);
-}
-
-void __LCD_DRIVER_WRITE_ONE_SIG_UP_AFTER() {
-	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
-}
-
-void __LCD_DRIVER_WRITE_ONE_SIG_DOWN() {
-	digitalWrite(0, 0);
-}
-
-void __LCD_DRIVER_WRITE_ONE_SIG_DOWN_AFTER() {
-	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
-}
-
-void __LCD_DRIVER_WRITE_RESET() {
-	digitalWrite(0, 0);
-}
+void __LCD_DRIVER_INIT();
+void __LCD_DRIVER_WRITE_ZERO_SIG_UP();
+void __LCD_DRIVER_WRITE_ZERO_SIG_UP_AFTER();
+void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN();
+void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN_AFTER();
+void __LCD_DRIVER_WRITE_ONE_SIG_UP();
+void __LCD_DRIVER_WRITE_ONE_SIG_UP_AFTER();
+void __LCD_DRIVER_WRITE_ONE_SIG_DOWN();
+void __LCD_DRIVER_WRITE_ONE_SIG_DOWN_AFTER();
+void __LCD_DRIVER_WRITE_RESET();
 void __LCD_DRIVER_WRITE_RESET_AFTER();
 
 //#endif
