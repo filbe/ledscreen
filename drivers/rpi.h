57e91d7540e083f50b4719b5db3ebf14a79c4c3d
V-P Lahti <vilp3@hotmail.com>
2019-11-17 18:30:12 +0200

    rpi first test

--- drivers/rpi.h
+++ drivers/rpi.h
@@ -5,43 +5,43 @@
 #include <wiringPi.h>
 
 
-inline void __LCD_DRIVER_INIT() {
+void __LCD_DRIVER_INIT() {
 	wiringPiSetup();
 	pinMode(0, OUTPUT);
 }
-inline void __LCD_DRIVER_WRITE_ZERO_SIG_UP() {
+void __LCD_DRIVER_WRITE_ZERO_SIG_UP() {
 	digitalWrite(0, 1);
 }
 
-inline void __LCD_DRIVER_WRITE_ZERO_SIG_UP_AFTER() {
+void __LCD_DRIVER_WRITE_ZERO_SIG_UP_AFTER() {
 	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
 }
 
-inline void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN() {
+void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN() {
 	digitalWrite(0, 0);
 }
 
-inline void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN_AFTER() {
+void __LCD_DRIVER_WRITE_ZERO_SIG_DOWN_AFTER() {
 	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
 }
 
-inline void __LCD_DRIVER_WRITE_ONE_SIG_UP() {
+void __LCD_DRIVER_WRITE_ONE_SIG_UP() {
 	digitalWrite(0, 1);
 }
 
-inline void __LCD_DRIVER_WRITE_ONE_SIG_UP_AFTER() {
+void __LCD_DRIVER_WRITE_ONE_SIG_UP_AFTER() {
 	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
 }
 
-inline void __LCD_DRIVER_WRITE_ONE_SIG_DOWN() {
+void __LCD_DRIVER_WRITE_ONE_SIG_DOWN() {
 	digitalWrite(0, 0);
 }
 
-inline void __LCD_DRIVER_WRITE_ONE_SIG_DOWN_AFTER() {
+void __LCD_DRIVER_WRITE_ONE_SIG_DOWN_AFTER() {
 	//__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");__asm("nop");
 }
 
-inline void __LCD_DRIVER_WRITE_RESET() {
+void __LCD_DRIVER_WRITE_RESET() {
 	digitalWrite(0, 0);
 }
 void __LCD_DRIVER_WRITE_RESET_AFTER();
