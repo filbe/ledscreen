910f4ff995b964796cb1503533ee6d32655dad2e
V-P Lahti <vilp3@hotmail.com>
2019-11-17 19:15:45 +0200

    rpi first test

--- drivers/rpi.c
+++ drivers/rpi.c
@@ -11,30 +11,13 @@
 #include <wiringPi.h>
 #include "rpi.h"
 
-
-int msleep(long msec)
-{
-    struct timespec ts;
-    int res;
-
-    if (msec < 0)
-    {
-        errno = EINVAL;
-        return -1;
-    }
-
-    ts.tv_sec = msec / 1000;
-    ts.tv_nsec = (msec % 1000) * 1000000;
-
-    do {
-        res = nanosleep(&ts, &ts);
-    } while (res && errno == EINTR);
-
-    return res;
-}
-
 void delayNanoseconds(unsigned int howLong) {
-	msleep(howLong);
+	struct timespec ts;
+	ts.tv_sec = 0;
+	ts.tv_nsec = howLong;
+
+	nanosleep(&ts, NULL);
+	
 }
 
 void __LCD_DRIVER_INIT() {
